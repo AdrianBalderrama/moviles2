3c8b46af4542176e7894c949b5130875a26ab6ef
Ivan Jimenez <ivan@localhost.localdomain>
2019-01-31 14:51:55 -0700

    jueves 31 de enero

---
+++ app1-fragmentos-genios/app/src/main/java/mx/ivancastro/app1_fragmentos_genios/MainActivity.kt
@@ -0,0 +1,12 @@
+package mx.ivancastro.app1_fragmentos_genios
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
